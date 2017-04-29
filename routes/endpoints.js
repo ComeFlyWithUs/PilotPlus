8f39da7c8beb0d7d3ff06332c6c7f9e50249a92e
verocoder <david@verophotography.co.uk>
2017-04-29 23:58:53 +0100

    added a getAirports method to API that takes a lat, long and radius parameter

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -14,6 +14,42 @@ router.get('/getPlaces', function (req, res, next) {
 
         res.json(response);
     });
+});
+
+// http://localhost:8081/api/getAirports?lat=50.727622&long=-3.475646&radius=50000
+router.get('/getAirports', function (req, res, next) {
+    var lat = req.param('lat');
+    var long = req.param('long');
+    var radius = req.param('radius');
+    console.log(lat);
+    console.log(long);
+    var baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    var key = "AIzaSyBA1clTuoA4xzkQaqYdmLlM5VDVQeue8sE";
+    var url = baseUrl + "?key=" + key + "&location=" + lat + ","+ long + "&radius=" + radius + "&type=airport";
+
+    var options = { method: 'GET',
+      url: url,
+      headers: { 'content-type': 'application/json' },
+      json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error)
+            throw new Error(error);
+            var listOfPlaces = [];
+            var results = response.body.results;
+            console.log(response);
+            console.log(results);
+            for (var i=0; i<results.length; i++){
+              var result = {};
+              result.name = results[i].name;
+              result.lat = results[i].geometry.location.lat;
+              result.long = results[i].geometry.location.lng;
+              listOfPlaces.push(result);
+            }
+        res.json(listOfPlaces);
+    });
+
 
 });
 
