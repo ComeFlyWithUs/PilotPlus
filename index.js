3e116be85dfe2d0cf1c2cc16f606e646679fc10e
Stewart A <strangelytyped+github-23f9bb@gmail.com>
2017-04-29 16:33:58 +0100

    Prep for heroku deploy

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express = require("express");
 var app = express();
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 app.use(express.static("static"));
 app.use("/lib", express.static("node_modules/cesium/Build"));
 
-app.listen(8080);
+app.listen(normalizePort(process.env.PORT || '8080'));
