b3fc1c038946fda398d9d67e481ea1d4ab11387e
Stewart A <strangelytyped+github-23f9bb@gmail.com>
2017-04-30 11:45:22 +0100

    Less markers

--- static/app.js
+++ static/app.js
@@ -194,7 +194,7 @@
         var obj = {};
         obj.lat = latLong.latitude * (180.0 / Math.PI);
         obj.long = latLong.longitude * (180.0 / Math.PI);
-        obj.radius = 50000;
+        obj.radius = 10000;
         var now = new Date().getTime();
         if(now - lastFetch > 10000){
           lastFetch = now;
